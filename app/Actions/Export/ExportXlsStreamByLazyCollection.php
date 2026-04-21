db14a9f28fb8c80a2f6f38aa85685f75aec91ed4
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-04-21 07:57:36 +0000

    Check & fix styling

--- app/Actions/Export/ExportXlsStreamByLazyCollection.php
+++ app/Actions/Export/ExportXlsStreamByLazyCollection.php
@@ -129,6 +129,7 @@ class ExportXlsStreamByLazyCollection
 
         /** @var array<string> $headers */
         $headers = array_values($headings->map(strval(...))->toArray());
+
         return $headers;
     }
 }
