419f817a12a726bf0129c1e64061bb73723f982c
marco76tv <marco.sottana@gmail.com>
2026-05-11 22:12:45 +0200

    .

--- app/Filament/Resources/CacheLockResource/Tables/CacheLocksTable.php
+++ app/Filament/Resources/CacheLockResource/Tables/CacheLocksTable.php
@@ -4,23 +4,17 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Resources\CacheLockResource\Tables;
 
-use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
 class CacheLocksTable extends XotBaseResourceTable
 {
-    /**
-     * @return array<string, Column>
-     */
     public static function getTableColumns(): array
     {
         return [
-            'key' => TextColumn::make('key')->searchable()->sortable(),
-            'owner' => TextColumn::make('owner')->searchable()->sortable(),
-            'expiration' => TextColumn::make('expiration')->sortable(),
-            'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
-            'updated_at' => TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+            'id' => TextColumn::make('id')->searchable()->sortable(),
+            'created_at' => TextColumn::make('created_at')->dateTime(),
+            'updated_at' => TextColumn::make('updated_at')->dateTime(),
         ];
     }
 }
---
+++ docs/wiki/concepts/INDEX.md
@@ -0,0 +1,17 @@
+# Xot Module - concepts Index
+
+## Purpose
+Index for Xot module concepts.
+
+## On-Demand Loading
+
+```bash
+qmd search "Xot concepts" --limit 5
+```
+
+## See Also
+- [Root Trigger Map](../../../../../docs/wiki/rules/00-TRIGGER_MAP.md)
+- [Root Wiki](../../../docs/wiki/)
+
+---
+*Updated: 2026-05-11*
