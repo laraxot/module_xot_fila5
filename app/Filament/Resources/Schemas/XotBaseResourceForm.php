556b7d0d842547b338ae80abc9ec5c19c33a2f94
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:05:12 +0200

    .

--- app/Filament/Resources/Schemas/XotBaseResourceForm.php
+++ app/Filament/Resources/Schemas/XotBaseResourceForm.php
@@ -4,66 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Resources\Schemas;
 
-use Filament\Schemas\Components\Component as SchemaComponent;
-use Filament\Schemas\Components\Wizard\Step;
-use Filament\Schemas\Schema;
-use Illuminate\Support\Str;
+use Filament\Forms\Components\Component;
 
-class XotBaseResourceForm
+abstract class XotBaseResourceForm
 {
-    public static function configure(Schema $schema): Schema
-    {
-        return $schema
-            ->components(static::getFormSchema())
-            ->columns(static::getFormSchemaColumns());
-    }
-
-    public static function getFormSchemaColumns(): int
-    {
-        return 1;
-    }
-
     /**
-     * @return array<int|string, SchemaComponent>
+     * @return array<int|string, Component>
      */
-    public static function getFormSchema(): array
-    {
-        return [
-        ];
-    }
-
-    public static function getWizardSteps(): array
-    {
-        return [];
-    }
-
-    protected static function getStepByName(string $name): Step
-    {
-        $methodName = Str::of($name)
-            ->snake()
-            ->studly()
-            ->prepend('get')
-            ->append('Schema')
-            ->toString();
-        $module_low = Str::of(static::class)->between('Modules\\', '\\Filament')->lower()->toString();
-        $group = Str::of(class_basename(static::class))->kebab()->toString();
-        $base_key = $module_low.'::'.$group.'.steps.';
-
-        $labelKey = $base_key.$name.'.label';
-        $descriptionKey = $base_key.$name.'.description';
-
-        if (method_exists(static::class, $methodName)) {
-            $schemaResult = static::$methodName();
-            /** @var array<int, SchemaComponent> $schemaComponents */
-            $schemaComponents = \is_array($schemaResult) ? array_values($schemaResult) : [];
-
-            return Step::make(__($labelKey))
-                ->label(__($labelKey))
-                ->description(__($descriptionKey))
-                ->schema($schemaComponents);
-        }
-        dddx($methodName);
-
-        return Step::make(__($labelKey))->schema([]);
-    }
+    abstract public static function getFormSchema(): array;
 }
--- app/Filament/Resources/Tables/XotBaseResourceTable.php
+++ app/Filament/Resources/Tables/XotBaseResourceTable.php
@@ -6,5 +6,8 @@ namespace Modules\Xot\Filament\Resources\Tables;
 
 abstract class XotBaseResourceTable
 {
+    /**
+     * @return array<int\|string, \Filament\Tables\Columns\Column>
+     */
     abstract public static function getTableColumns(): array;
 }
