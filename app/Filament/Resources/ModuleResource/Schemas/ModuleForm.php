cfb5c8a175faac338135db975c40a8aca2551432
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:12:01 +0200

    .

--- app/Filament/Resources/ModuleResource/Schemas/ModuleForm.php
+++ app/Filament/Resources/ModuleResource/Schemas/ModuleForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Xot\app\Filament\Resources\ModuleResource\Schemas;
+namespace Modules\Xot\app\Filament\Resources\ModuleResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
