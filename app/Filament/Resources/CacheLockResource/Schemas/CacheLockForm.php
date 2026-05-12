df9569edddb16178109c78213038efa57e979a2c
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:05:12 +0200

    .

--- app/Filament/Resources/CacheLockResource/Schemas/CacheLockForm.php
+++ app/Filament/Resources/CacheLockResource/Schemas/CacheLockForm.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace Modules\Xot\Filament\Resources\CacheLockResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Component as \\Filament\\Forms\\Components\\Component;
 use Modules\Xot\Filament\Resources\Schemas\XotBaseResourceForm;
 
 class CacheLockForm extends XotBaseResourceForm
 {
     /**
-     * @return array<int|string, \\Filament\\Forms\\Components\\Component>
+     * @return array<int|string, \Filament\Forms\Components\Component>
      */
     public static function getFormSchema(): array
     {
