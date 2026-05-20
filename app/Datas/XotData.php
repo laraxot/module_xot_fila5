f2e24608821ff525fca209a64205062681e58e4e
marco76tv <marco.sottana@gmail.com>
2026-05-20 21:36:02 +0200

    .

--- app/Datas/XotData.php
+++ app/Datas/XotData.php
@@ -119,6 +119,7 @@ class XotData extends Data implements Wireable
         );
         Assert::isAOf($class, Model::class, '['.__LINE__.']['.class_basename($this).']['.$class.']');
 
+        /** @var class-string<Model&UserContract> $class */
         return $class;
     }
 
@@ -159,7 +160,10 @@ class XotData extends Data implements Wireable
             '['.$this->team_class.']['.__LINE__.']['.class_basename($this).']',
         );
 
-        return $this->team_class;
+        /** @var class-string<Model&TeamContract> $teamClass */
+        $teamClass = $this->team_class;
+
+        return $teamClass;
     }
 
     /**
@@ -186,7 +190,10 @@ class XotData extends Data implements Wireable
             '['.__LINE__.']['.class_basename($this).']['.$this->tenant_class.']',
         );
 
-        return $this->tenant_class;
+        /** @var class-string<Model&TenantContract> $tenantClass */
+        $tenantClass = $this->tenant_class;
+
+        return $tenantClass;
     }
 
     /**
@@ -383,6 +390,7 @@ class XotData extends Data implements Wireable
             '['.__LINE__.']['.class_basename($this).']['.$class.']',
         );
 
+        /** @var class-string<Model&UserContract> $class */
         return $class;
     }
 
