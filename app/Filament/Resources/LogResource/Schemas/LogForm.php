7a4069a52ae43d81cbfde833179477c94983fe8e
marco76tv <marco.sottana@gmail.com>
2026-05-21 12:14:38 +0200

    .

--- app/Filament/Resources/LogResource/Schemas/LogForm.php
+++ app/Filament/Resources/LogResource/Schemas/LogForm.php
@@ -13,6 +13,7 @@ class LogForm extends XotBaseResourceForm
     /**
      * @return array<int|string, \Filament\Schemas\Components\Component>
      */
+
     public static function getFormSchema(): array
     {
         return [
