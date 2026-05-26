05a8ba33a8bfbfb522e00f1c4aa16aee41fb29d8
marco76tv <marco.sottana@gmail.com>
2026-05-26 14:03:40 +0200

    .

--- app/Filament/Resources/CacheResource/Tables/CachesTable.php
+++ app/Filament/Resources/CacheResource/Tables/CachesTable.php
@@ -9,7 +9,7 @@ use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
 class CachesTable extends XotBaseResourceTable
 {
-    public static function getTableColumns(): array
+    public function getTableColumns(): array
     {
         /*
          * @return array<int|string, \Filament\Tables\Columns\Column>
