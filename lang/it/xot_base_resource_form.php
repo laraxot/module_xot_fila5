dc3645ae54a0f91430269aaf735624df0a71b422
marco76tv <marco.sottana@gmail.com>
2026-06-17 15:37:43 +0200

    .

--- lang/it/xot_base_resource_form.php
+++ lang/it/xot_base_resource_form.php
@@ -14,4 +14,48 @@ return [
             'label' => 'summary',
         ],
     ],
+    'fields' => [
+        'id' => [
+            'label' => 'id',
+            'placeholder' => 'id',
+            'helper_text' => 'id',
+            'description' => 'id',
+        ],
+        'name' => [
+            'label' => 'name',
+            'placeholder' => 'name',
+            'helper_text' => 'name',
+            'description' => 'name',
+        ],
+        'field_name' => [
+            'label' => 'field_name',
+            'placeholder' => 'field_name',
+            'helper_text' => 'field_name',
+            'description' => 'field_name',
+        ],
+        'op' => [
+            'label' => 'op',
+            'placeholder' => 'op',
+            'helper_text' => 'op',
+            'description' => 'op',
+        ],
+        'value' => [
+            'label' => 'value',
+            'placeholder' => 'value',
+            'helper_text' => 'value',
+            'description' => 'value',
+        ],
+        'type' => [
+            'label' => 'type',
+            'placeholder' => 'type',
+            'helper_text' => 'type',
+            'description' => 'type',
+        ],
+        'anno' => [
+            'label' => 'anno',
+            'placeholder' => 'anno',
+            'helper_text' => 'anno',
+            'description' => 'anno',
+        ],
+    ],
 ];
