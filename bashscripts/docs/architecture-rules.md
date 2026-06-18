36d6d1bf81668e69d29aa4c06bbce785ece7f5db
marco76tv <marco.sottana@gmail.com>
2026-06-18 21:33:16 +0200

    .

--- bashscripts/docs/architecture-rules.md
+++
@@ -1,7 +0,0 @@
----
-module: theme
-topic: architecture-rules
-canonical: ../../../../Themes/docs/shared-components/architecture-rules-Modules.md
----
-
-See canonical documentation: ../../../../Themes/docs/shared-components/architecture-rules-Modules.md
--- bashscripts/docs/fix_all_modules_docs_naming.sh
+++
@@ -1,7 +0,0 @@
----
-module: theme
-topic: fix_all_modules_docs_naming
-canonical: ../../../../Themes/docs/shared-components/fix-all-modules-docs-naming.sh
----
-
-See canonical documentation: ../../../../Themes/docs/shared-components/fix-all-modules-docs-naming.sh
