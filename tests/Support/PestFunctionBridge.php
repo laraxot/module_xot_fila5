1cc9791314c1c324980de0831b624e7a74c983f7
marco76tv <marco.sottana@gmail.com>
2026-07-01 10:43:46 +0200

    .

--- tests/Support/PestFunctionBridge.php
+++ tests/Support/PestFunctionBridge.php
@@ -4404,9 +4404,6 @@ function uses(string ...$classes): void
 function skip(mixed ...$arguments): void
 {
 }
-<<<<<<< HEAD
-
-=======
 namespace Modules\Job\Tests\Unit\Traits;
 
 use Closure;
@@ -4467,7 +4464,6 @@ function uses(string ...$classes): void
 function skip(mixed ...$arguments): void
 {
 }
->>>>>>> 7949283d (.)
 namespace Modules\Lang\Tests;
 
 use Closure;
@@ -12213,9 +12209,6 @@ function uses(string ...$classes): void
 function skip(mixed ...$arguments): void
 {
 }
-<<<<<<< HEAD
-
-=======
 namespace Modules\Xot\Tests\Unit\Actions\Array;
 
 use Closure;
@@ -12276,7 +12269,6 @@ function uses(string ...$classes): void
 function skip(mixed ...$arguments): void
 {
 }
->>>>>>> 7949283d (.)
 namespace Modules\Xot\Tests\Unit\Actions\Blade;
 
 use Closure;
