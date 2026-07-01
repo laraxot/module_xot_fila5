80c65f5afb3a08cbdca3af9ac67c4e3558907fe4
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-07-01 14:06:31 +0000

    Check & fix styling

--- app/Mixins/PanelMixin.php
+++ app/Mixins/PanelMixin.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Mixins;
 
-use Closure;
-use Filament\Panel;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
@@ -15,101 +13,106 @@ use Nwidart\Modules\Module as NwidartModule;
 use Webmozart\Assert\Assert;
 
 /**
- * @method string getId()
- * @method string getName()
+ * @method string        getId()
+ * @method string        getName()
  * @method NwidartModule getModule()
- * @method array getConfig()
- * @method array getModuleConfig()
- * @method string getNavigationLabel()
- * @method string getNavigationIcon()
- * @method int getNavigationSort()
+ * @method array         getConfig()
+ * @method array         getModuleConfig()
+ * @method string        getNavigationLabel()
+ * @method string        getNavigationIcon()
+ * @method int           getNavigationSort()
  */
 class PanelMixin
-{   
-    
-   
-   /**
-     * @return Closure
+{
+    /**
+     * @return \Closure
      */
-    public function getName() {
+    public function getName()
+    {
         return function (): string {
-        
-            $id=$this->getId();
-            $name=Str::before($id,'::');
+            $id = $this->getId();
+            $name = Str::before($id, '::');
+
             return $name;
         };
     }
 
-     /**
-     * @return Closure
+    /**
+     * @return \Closure
      */
-    public function getModule(){
+    public function getModule()
+    {
         return function (): NwidartModule {
-         $name=$this->getName();
-         $module=Module::find($name);
-         return $module;
-       };
-    }
+            $name = $this->getName();
+            $module = Module::find($name);
 
-     /**
-     * @return Closure
-     */
-    public function getConfig() {
-        return function (): array {
-            $name=$this->getName();
-            $config=Config::array($name);
-            return $config;
-       };
+            return $module;
+        };
     }
 
     /**
-     * @return Closure
+     * @return \Closure
      */
-    public function getModuleConfig() {
+    public function getConfig()
+    {
         return function (): array {
-            $module=$this->getModule();
-            $configFilePath=$module->getPath().'/config/config.php';
+            $name = $this->getName();
+            $config = Config::array($name);
+
+            return $config;
+        };
+    }
+
+    /**
+     * @return \Closure
+     */
+    public function getModuleConfig()
+    {
+        return function (): array {
+            $module = $this->getModule();
+            $configFilePath = $module->getPath().'/config/config.php';
             $config = File::getRequire($configFilePath);
-            Assert::isArray($config,'['.__LINE__.']['.class_basename($this).']');
+            Assert::isArray($config, '['.__LINE__.']['.class_basename($this).']');
+
             return $config;
-       };
+        };
     }
 
-
     /**
-     * @return Closure
+     * @return \Closure
      */
-    public function getNavigationLabel() {
+    public function getNavigationLabel()
+    {
         return function (): string {
-            $config=$this->getModuleConfig();
-            $name=Arr::get($config,'name');
-            Assert::string($name,'['.__LINE__.']['.class_basename($this).']');
-            
-            
+            $config = $this->getModuleConfig();
+            $name = Arr::get($config, 'name');
+            Assert::string($name, '['.__LINE__.']['.class_basename($this).']');
+
             return $name;
         };
     }
 
     /**
-     * @return Closure
+     * @return \Closure
      */
-    public function getNavigationIcon() {
+    public function getNavigationIcon()
+    {
         return function (): string {
-            $config=$this->getModuleConfig();
-            $icon=Arr::get($config,'icon');
-            Assert::string($icon,'['.__LINE__.']['.class_basename($this).']');
-            
+            $config = $this->getModuleConfig();
+            $icon = Arr::get($config, 'icon');
+            Assert::string($icon, '['.__LINE__.']['.class_basename($this).']');
+
             return $icon;
         };
     }
 
     /**
-     * @return Closure
+     * @return \Closure
      */
-    public function getNavigationSort() {
+    public function getNavigationSort()
+    {
         return function (): int {
             return 0;
         };
     }
-   
 }
