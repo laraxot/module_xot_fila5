6c0131bcc94408d8f12882a3828f853b14d0b30a
marco76tv <1944941+marco76tv@users.noreply.github.com>
2026-07-24 10:32:55 +0000

    Check & fix styling

--- app/Filament/Pages/Tenancy/XotBaseEditTenantProfile.php
+++ app/Filament/Pages/Tenancy/XotBaseEditTenantProfile.php
@@ -12,4 +12,6 @@ use Filament\Pages\Tenancy\EditTenantProfile as FilamentEditTenantProfile;
  * Following Laraxot architectural pattern: never extend Filament classes directly.
  * This class wraps Filament's EditTenantProfile to provide a XotBase layer.
  */
-abstract class XotBaseEditTenantProfile extends FilamentEditTenantProfile {}
+abstract class XotBaseEditTenantProfile extends FilamentEditTenantProfile
+{
+}
