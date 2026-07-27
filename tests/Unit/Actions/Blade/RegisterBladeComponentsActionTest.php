3ff16257e7614eacdaf40577dfe8e489f511510f
marco76tv <marco.sottana@gmail.com>
2026-07-27 16:05:53 +0200

    .

--- tests/Unit/Actions/Blade/RegisterBladeComponentsActionTest.php
+++ tests/Unit/Actions/Blade/RegisterBladeComponentsActionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Tests\Unit\Actions\Blade;
 
+<<<<<<< HEAD
 use Illuminate\Support\Facades\Blade;
 use Mockery;
 use Modules\Xot\Actions\Blade\RegisterBladeComponentsAction;
@@ -17,12 +18,22 @@ uses(TestCase::class);
 afterEach(function (): void {
     \Mockery::close();
 });
+=======
+use Mockery;
+use Mockery\MockInterface;
+use Modules\Xot\Actions\Blade\RegisterBladeComponentsAction;
+use Modules\Xot\Actions\File\GetComponentsAction;
+use Modules\Xot\Datas\ComponentFileData;
+use PHPUnit\Framework\Assert;
+use Spatie\LaravelData\DataCollection;
+>>>>>>> ef8836ec (.)
 
 it('registers blade components correctly', function (): void {
     $path = 'some/path';
     $namespace = 'Some\\Namespace';
     $prefix = 'prefix';
 
+<<<<<<< HEAD
     $comp1 = ComponentFileData::from([
         'name' => 'test-comp',
         'ns' => 'Some\\Namespace\\View\\Components\\TestComp',
@@ -44,8 +55,26 @@ it('registers blade components correctly', function (): void {
     Blade::shouldReceive('component')
         ->once()
         ->with('test-comp', 'Some\\Namespace\\View\\Components\\TestComp');
+=======
+    /** @var DataCollection<int, ComponentFileData> $mockComps */
+    $mockComps = ComponentFileData::collection([
+        [
+            'name' => 'test-comp',
+            'ns' => 'Some\\Namespace\\View\\Components\\TestComp',
+            'class' => 'TestComp',
+        ],
+    ]);
+
+    /** @var GetComponentsAction&MockInterface $getComponents */
+    $getComponents = Mockery::mock(GetComponentsAction::class);
+    $getComponents->allows(['execute' => $mockComps]);
+    app()->instance(GetComponentsAction::class, $getComponents);
+>>>>>>> ef8836ec (.)
 
     $action = app(RegisterBladeComponentsAction::class);
+    Assert::assertInstanceOf(RegisterBladeComponentsAction::class, $action);
+    Assert::assertSame(1, $mockComps->count());
+
     $action->execute($path, $namespace, $prefix);
 });
 
@@ -53,6 +82,7 @@ it('does nothing if no components found', function (): void {
     $path = 'empty/path';
     $namespace = 'Empty\\Namespace';
 
+<<<<<<< HEAD
     $mockComps = new DataCollection(ComponentFileData::class, []);
 
     $mock = \Mockery::mock(GetComponentsAction::class);
@@ -63,7 +93,19 @@ it('does nothing if no components found', function (): void {
     app()->instance(GetComponentsAction::class, $mock);
 
     Blade::shouldReceive('component')->never();
+=======
+    /** @var DataCollection<int, ComponentFileData> $mockComps */
+    $mockComps = ComponentFileData::collection([]);
+
+    /** @var GetComponentsAction&MockInterface $getComponents */
+    $getComponents = Mockery::mock(GetComponentsAction::class);
+    $getComponents->allows(['execute' => $mockComps]);
+    app()->instance(GetComponentsAction::class, $getComponents);
+>>>>>>> ef8836ec (.)
 
     $action = app(RegisterBladeComponentsAction::class);
+    Assert::assertInstanceOf(RegisterBladeComponentsAction::class, $action);
+    Assert::assertSame(0, $mockComps->count());
+
     $action->execute($path, $namespace);
 });
